51580aca840c3f77e039b73f391d8bae3b5e81cc
lkj1313 <108466769+lkj1313@users.noreply.github.com>
2026-07-02 20:22:30 +0900

    Time: 0 ms (100%), Space: 53.3 MB (86.4%) - LeetHub

---
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var maxDepth = function(s) {
+    let count = 0;
+    let maxCount = 0;
+
+    for(let i=0; i<s.length; i++){
+        if(s[i]==='('){
+            count++;
+            maxCount = Math.max(count,maxCount);
+        } else if(s[i]===')'){
+            count--;
+        }
+    }
+    return maxCount;
+};
