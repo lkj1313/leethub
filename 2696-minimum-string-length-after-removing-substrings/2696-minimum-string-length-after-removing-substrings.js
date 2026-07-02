f543510b8cf852aa9473c880135f1f2ecc254700
lkj1313 <108466769+lkj1313@users.noreply.github.com>
2026-07-02 20:55:53 +0900

    Time: 4 ms (49.58%), Space: 56.3 MB (84.87%) - LeetHub

---
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var minLength = function(s) {
+    const stack = [];
+
+    for(let i=0; i<s.length; i++){
+        if(s[i]==='B' && stack[stack.length-1] === "A"){
+            stack.pop();
+        } else if(s[i]==='D' && stack[stack.length-1] === "C"){
+            stack.pop();
+        } else{
+            stack.push(s[i]);
+        }
+        
+    }
+
+    return stack.length;
+};
