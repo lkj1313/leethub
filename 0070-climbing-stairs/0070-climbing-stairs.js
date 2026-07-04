e5ec6cc7c918105191662e96262ccb8d61be8048
lkj1313 <108466769+lkj1313@users.noreply.github.com>
2026-07-04 20:20:41 +0900

    Time: 0 ms (100%), Space: 54.2 MB (12.41%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    const dp = new Array(n+1).fill(0);
+    dp[1] = 1;
+    dp[2] = 2;
+    dp[3] = 3;
+
+    for(let i=4; i<=n; i++){
+        dp[i] = dp[i-2] + dp[i-1]
+    }
+    return dp[n];
+};
